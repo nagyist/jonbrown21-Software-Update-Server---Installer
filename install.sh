e98b36b4472596e0f854eb7d35f478821ade3e42
Jon Brown <jonbrown2@mac.com>
2013-11-09 19:35:17 -0500

    Changes

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-read -p "What would you like to do (install/uninstall)? "
-
-[ "$REPLY" != "install" ] || exec /SUS-Installer/src/install.sh
-[ "$REPLY" != "uninstall" ] || exec /SUS-Installer/src/uninstall.sh
- 
--- src/uninstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Starting the uninstall process"
-
-sudo rm /Library/Preferences/com.apple.SoftwareUpdate.plist
-
-echo "Uninstall Complete!"
-
-exit 0
